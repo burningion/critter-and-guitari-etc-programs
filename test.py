7ac94693d6c32e053f498f71a20e42a5a2a8bb10
kbsezginel <kbsezginel@othot.com>
2020-02-21 13:48:03 -0500

    Update test mode to be reactive to knob changes

--- test.py
+++ test.py
@@ -1,11 +1,31 @@
+"""
+Draw a restless circle.
+
+* Use with Auto Clear Toggle Off
+Knob 1: Circle restlessness (position randomness)
+Knob 2: Max circle size
+Knob 3: Circle fade
+Knob 4: Circle color
+Knob 5: Background color
+"""
+import os
 import pygame
+import random
+
 
 def setup(screen, etc):
     pass
 
 def draw(screen, etc):
-    pygame.gfxdraw.filled_circle(screen, 
-                                 1280 // 2,
-                                 720 // 2,
-                                 50, 
-                                 (128, 254, 3))
+    size = int(etc.knob2 * 360)
+    x = int(640 + (random.random() - 0.5) * 1280 * etc.knob1)
+    y = int(360 + (random.random() - 0.5) * 720 * etc.knob1)
+    color = etc.color_picker()
+    pygame.draw.circle(screen, color, (x, y), size)
+
+    #TRAILS
+    veil = pygame.Surface((1280, 720))
+    veil.set_alpha(int(etc.knob3 * 200)) # adjust transparency on knob3
+    bg_color = etc.color_picker_bg()
+    veil.fill((bg_color[0], bg_color[1], bg_color[2]))
+    screen.blit(veil, (0, 0)) # (0,0) = starts at top left
